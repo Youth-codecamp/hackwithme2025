357a6a84f572b1774b8f7e208977f42e8be38235
Shyaka Chaste <liochastej@gmail.com>
2024-11-04 19:06:18 +0000

    starting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome To Hack With Me</h1>
+    </div>
+  );
+}
+
+export default App;
