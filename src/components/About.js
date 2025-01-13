efbe7067c43e1be81ae6b51c619158a12918e31f
Shyaka Chaste <liochastej@gmail.com>
2025-01-13 23:22:06 +0200

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ import './About.css';
 const About = () => {
   return (
     <section className="about-page" id="about">
-      <motion.h1 
+      <motion.h1
         className="about-title"
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
@@ -13,7 +13,7 @@ const About = () => {
       >
         About Hack With Me 2025
       </motion.h1>
-      <motion.p 
+      <motion.p
         className="about-paragraph"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -22,6 +22,23 @@ const About = () => {
         Hack With Me is a yearly, 24-hour coding hackathon for high school students that happens on August 16th. Students join to create projects, solve problems, and improve their coding skills. The goal of Hack With Me is to make hackathons accessible to everyone. 
         That’s why this year, we aim to bring together 200 high school students to share their ideas and showcase their talents.
       </motion.p>
+
+      <motion.div
+        className="spots-container"
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, delay: 1 }}
+      >
+        <h2 className="spots-title">Spots Remaining</h2>
+        <motion.div
+          className="spots-number"
+          initial={{ y: -10 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.6, yoyo: Infinity }}
+        >
+          200
+        </motion.div>
+      </motion.div>
     </section>
   );
 };
